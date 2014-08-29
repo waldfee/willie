dbf31688e561ad5e3931bf93daa1bc383a5ce037
waldfee <waldfee@users.noreply.github.com>
2014-08-29 10:11:56 +0200

    Added openweather module
    This module fetches weather conditions from openweathermap.org in metric
    units.

---
+++ willie/modules/openweather.py
@@ -0,0 +1,35 @@
+# coding=utf8
+"""
+openweather.py - Willie openweathermap.com module
+"""
+from __future__ import unicode_literals
+import json
+from willie import web
+from willie.module import commands, example, NOLIMIT
+
+
+@commands('ow')
+@example('.ow London')
+def weather(bot, trigger):
+    location = trigger.group(2)
+
+    if not location:
+        return bot.say("No location provided.")
+
+    try:
+        url = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + web.urlencode(location)
+        response = web.get(url, dont_decode=True)
+    except Exception:
+        return bot.say("Error while fetching data.")
+
+    data = json.loads(response)
+    if data["cod"] != "200":
+        return bot.say(data["message"])
+
+    name = data["name"]
+    country = data["sys"]["country"]
+    temp = data["main"]["temp"]
+    windspeed = data["wind"]["speed"]
+    condition = data["weather"][0]["main"]
+
+    return bot.say(u'%s, %s: %s, %s°, %skmh' % (name, country, condition, temp, windspeed))
