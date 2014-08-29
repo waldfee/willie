69aeb4e23d4530a87bdd4fff169d1e0c3e16ade8
waldfee <waldfee@users.noreply.github.com>
2014-08-29 10:34:51 +0200

    Fixed openweather module errors

--- willie/modules/openweather.py
+++ willie/modules/openweather.py
@@ -4,8 +4,9 @@ openweather.py - Willie openweathermap.com module
 """
 from __future__ import unicode_literals
 import json
+
 from willie import web
-from willie.module import commands, example, NOLIMIT
+from willie.module import commands, example
 
 
 @commands('ow')
@@ -17,13 +18,13 @@ def weather(bot, trigger):
         return bot.say("No location provided.")
 
     try:
-        url = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + web.urlencode(location)
+        url = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=" + location
         response = web.get(url, dont_decode=True)
     except Exception:
         return bot.say("Error while fetching data.")
 
     data = json.loads(response)
-    if data["cod"] != "200":
+    if data["cod"] != 200:
         return bot.say(data["message"])
 
     name = data["name"]
