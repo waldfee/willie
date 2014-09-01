c2a22ec8807907eecb56869711089440a2ecee4c
waldfee <waldfee@users.noreply.github.com>
2014-09-01 10:44:45 +0200

    Added forecasts to openweather module

--- willie/modules/openweather.py
+++ willie/modules/openweather.py
@@ -3,6 +3,7 @@
 openweather.py - Willie openweathermap.com module
 """
 from __future__ import unicode_literals
+from datetime import datetime
 import json
 
 from willie import web
@@ -34,3 +35,42 @@ def weather(bot, trigger):
     condition = data["weather"][0]["main"]
 
     return bot.say(u'%s, %s: %s, %s°, %skmh' % (name, country, condition, temp, windspeed))
+
+
+@commands('of')
+@example('.of London')
+def forecast(bot, trigger):
+    location = trigger.group(2)
+
+    if not location:
+        return bot.say("No location provided.")
+
+    try:
+        url = "http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=3&q=" + location
+        response = web.get(url, dont_decode=True)
+    except Exception:
+        return bot.say("Error while fetching data.")
+
+    data = json.loads(response)
+    if data["cod"] != "200":
+        if data["message"] == "":
+            return bot.say("Error")
+        else:
+            return bot.say(data["message"])
+
+    name = data["city"]["name"]
+    country = data["city"]["country"]
+
+    day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+    for item in data["list"]:
+        weekday = datetime.datetime.fromtimestamp(item["dt"]).weekday()
+        minTemp = item["temp"]["min"]
+        maxTemp = item["temp"]["max"]
+        conditions = item["weather"]["main"]
+        windSpeed = item["speed"]
+
+        bot.say(u'%s, %s on %s: min: %s°, max: %s°, %s, %skmh' % (
+            name, country, day[weekday], minTemp, maxTemp, conditions, windSpeed))
+
+    return
